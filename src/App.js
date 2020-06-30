7d5265c559970de113ae498d43f5695aaf82d39c
Pronnoy <pronnoy1998@gmail.com>
2020-07-01 01:42:04 +0530

    Added styles and background color

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css'
 
 class App extends Component{
     constructor(){
@@ -12,16 +13,19 @@ class App extends Component{
         }
     }
     
-    onSearchChange(event){
-        console.log(event.target.value);
+    onSearchChange = (event) =>{
+        this.setState({ searchfield: event.target.value })
     }
 
     render(){
+        const filteredRobots = this.state.robots.filter(robots => {
+        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        })
         return (
             <div className="tc">
-                <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots={robots} />
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange= {this.onSearchChange}/>
+                <CardList robots={filteredRobots} />
             </div>
         );
     }
