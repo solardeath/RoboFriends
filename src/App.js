181c53f99e713ecc88a051e701c0b92873e76e1f
Pronnoy <pronnoy1998@gmail.com>
2020-06-30 20:05:46 +0530

    Made al my robot friends through Card Components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    
+    onSearchChange(event){
+        console.log(event.target.value);
+    }
+
+    render(){
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <CardList robots={robots} />
+            </div>
+        );
+    }
+    
+}
+
+export default App
